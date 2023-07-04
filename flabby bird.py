429f5a3a43f5e89deaefdb8bb695d87613670a86
advay <134825013+advay-c@users.noreply.github.com>
2023-07-03 21:37:05 -0400

    Update flabby bird.py

--- flabby bird.py
+++ flabby bird.py
@@ -5,8 +5,8 @@ import random
 pygame.font.init()
 
 WIDTH, HEIGHT = 609, 663 #sets the width and height of the window
-BIRD_HEIGHT = 115
-BIRD_WIDTH = 115
+BIRD_HEIGHT = 110
+BIRD_WIDTH = 110
 VEL = 5
 PIPE_WIDTH = 100
 pipe_vel = 6  # controls pipe and game speed
